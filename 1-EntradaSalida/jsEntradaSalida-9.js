54740ca2d2875f036497ae154e440031b97bae77
ianchare <47337950+ianchare@users.noreply.github.com>
2019-02-06 22:18:00 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,23 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+var sueldo;
+var .
 
-	
+.
+;
+var resultado;
+
+num1 = parseInt(document.getElementById("sueldo").value);
+
+num2 = parseInt(document.getElementById("resultado").value);
+
+resultado = (num1* 0,10);
+
+alert("el aumento es + num2 + resultado");
+
+
+
 }
+
+
