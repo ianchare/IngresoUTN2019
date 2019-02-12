7db5c6b9cb9e386be2853a8573291f2127737e71
ianchare <47337950+ianchare@users.noreply.github.com>
2019-02-11 22:25:43 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,16 @@
 function Mostrar()
 {
-//tomo la edad  
+//tomo la edad
+var edad;
 
+edad = parseInt(document.getElementById("edad").value);
+
+if(edad==15){
+
+alert("niña bonita");
+}
+
+alert("no es la edad")
 
 
 }//FIN DE LA FUNCIÓN
