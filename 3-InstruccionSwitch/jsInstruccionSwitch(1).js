7387cc34d7eab914c0e4a2d84d2b040b0c3a8ee3
ianchare <47337950+ianchare@users.noreply.github.com>
2019-02-18 22:08:32 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,9 +1,26 @@
 function Mostrar()
 {
 //tomo el mes
-var mesDelAño = document.getElementById('mes').value;
+var mesDelAnio = document.getElementById("mes").value;
 
+switch (mesDelAnio){
 
+  case "Enero" :
+  alert("que comiences bien el año!!");
+  break;
 
+  case "Marzo" :
+  alert("a clases!!!");
+  break;
+
+  case "Julio" :
+  alert( "se vienen las vacaciones!!!.");
+  break;
+
+  case "Diciembre" :
+  alert("Felices fiesta!!!.");
+  break;
+
+}
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,9 +1,37 @@
 function Mostrar()
 {
-//tomo el mes 
-var mesDelAño = document.getElementById('mes').value;
+//tomo el mes
+var mesDelAnio = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch (mesDelAnio){
+
+  case "Enero":
+  alert("aun no llega el invierno");
+  break;
+
+  case "Enero":
+  alert("aun no llega el invierno");
+  break
+
+  case "Enero":
+  alert("aun no llega el invierno");
+  break
+
+
+  case "Julio":
+  alert("Abrigate que hace frio");
+  break;
+
+  case "Agosto":
+  alert("Abrigate que hace frio");
+  break;
+
+  default:
+  alert("no estamos en invierno");
+
+}
+
+
 
 
 
