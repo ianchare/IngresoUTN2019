09b3dbc2f0f559941f01287144d6de53a606d6ea
ianchare <47337950+ianchare@users.noreply.github.com>
2019-02-20 21:58:51 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -3,5 +3,13 @@ function Mostrar()
 
 var clave = prompt("ingrese el número clave.");
 
+while(clave!="utn750"){
+
+  clave = prompt("clave incorrecta; reingresela de nuevo");
+
+}
+
+alert("clave correcta");
+
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -3,8 +3,12 @@ function Mostrar()
 
 var sexo = prompt("ingrese f ó m .");
 
+while(sexo == f){
 
+   = prompt("femenino");
 
-document.getElementById('Sexo').value=sexo;
+}
+
+document.getElementById("Sexo").value = sexo;
 
 }//FIN DE LA FUNCIÓN
